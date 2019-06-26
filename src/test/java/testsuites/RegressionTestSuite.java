7fee910c1fb1a0bf8865490edb92539226e5d1ea
Pannuvia Monteiro <pannuviam@dbserver.com.br>
2019-06-26 12:34:22 -0300

    Adição do arquivo RegressionTestSuite.java

--- src/test/java/testsuites/RegressionTestSuite.java
+++ src/test/java/testsuites/RegressionTestSuite.java
@@ -1,5 +1,28 @@
 package testsuites;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import testcases.ComprarProdutoComSucessoTestCase;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	ComprarProdutoComSucessoTestCase.class, 
+
+})
 public class RegressionTestSuite {
+
+	@BeforeClass
+	public static void setUp() {
+		System.out.println("Iniciando execução do teste de regressão do site Automation Practice");
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.out.println("Finalizando teste de regressão do site Automation Practice");
+	}
 
 }
